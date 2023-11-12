33072cff8c9a1f8c5b86a24aaf35e2b366a02c41
S4LPICON <s4lpicong4mer3993@gmail.com>
2023-11-12 12:10:44 -0500

    mas ejercicios y archivos vacios para mas placer xd

--- Ejercicios Fundamentos Ing sistemas/f_cuadratica.py
+++ Ejercicios Fundamentos Ing sistemas/f_cuadratica.py
@@ -1,2 +1,8 @@
+"""
+Calcular la imagen de un número real x mediante la función f(x)=3x2 -2x +1
+
+ejemplos: f_cuadratica(0) retorna 1
+                f_cuadratica(1) retorna 0
+"""
 def f_cuadratica(x):
     return 3 * (x*x) - (2 * x) + 1
