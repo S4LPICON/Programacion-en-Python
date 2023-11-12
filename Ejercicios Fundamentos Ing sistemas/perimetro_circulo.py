b9805689669602968cc91e4083a2dc3acaa1ab17
S4LPICON <s4lpicong4mer3993@gmail.com>
2023-11-12 12:14:23 -0500

    ediciones

--- Ejercicios Fundamentos Ing sistemas/perimetro_circulo.py
+++ Ejercicios Fundamentos Ing sistemas/perimetro_circulo.py
@@ -1,3 +1,11 @@
+"""
+Calcular el perímetro de un círculo P=2Π r , tomando un valor de pi=3.14159265358979323846
+
+ejemplos:
+
+       perimetro_circulo(1) retorna
+       perimetro_circulo(2.7 )
+"""
 def perimetro_circulo(x):
     pi=3.14159265358979323846
     return 2 * pi * x
