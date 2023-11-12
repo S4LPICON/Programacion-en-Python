891e8c2b09cc4f65489077237fac5c09f2282fd1
S4LPICON <s4lpicong4mer3993@gmail.com>
2023-11-12 12:12:50 -0500

    ediciones

--- Ejercicios Fundamentos Ing sistemas/area_circulo.py
+++ Ejercicios Fundamentos Ing sistemas/area_circulo.py
@@ -1,3 +1,10 @@
+"""
+Calcular el área de un círculo sabiendo que A=Π r2 , tomando un valor de pi=3.14159265358979323846
+
+ejemplos:
+
+       area_circulo(1) retorna 3.14159265358979323846
+"""
 def area_circulo(x):
     pi= 3.14159265358979323846
     return pi * x * x
