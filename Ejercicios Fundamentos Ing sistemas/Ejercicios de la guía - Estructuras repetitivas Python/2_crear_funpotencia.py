de89c64b419add4b691b3b5565ddd475b9a9b0e0
S4LPICON <s4lpicong4mer3993@gmail.com>
2023-11-13 15:02:31 -0500

    otros ejercicios realizados

---
+++ Ejercicios Fundamentos Ing sistemas/Ejercicios de la guía - Estructuras repetitivas Python/2_crear_funpotencia.py
@@ -0,0 +1,24 @@
+"""
+Cree la función Potencia 
+
+
+el nombre de la función debe ser potencia(2,3) donde el primer parámetro es la base y el segundo el exponente 
+"""
+def potencia(x,y):
+    i = 1
+    rta = x
+    if x == 0 and y != 0:
+        rta = 0
+    elif x != 0 and y == 0:
+        rta = 1
+    elif y > 0:
+        while i < y:
+            rta = rta * x
+            i = i+1
+    elif y < 0:
+        1/x-y
+    else: 
+        rta = "no definido"
+    return rta
+
+print(potencia(16,5))
